eb66161cbad781dd7290918efe30c2401deef849
cfailde <carlosfailde@gmail.com>
2026-09-08 10:41:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-var GHPATH = '/github-page-pwa';
-var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_002';
+var GHPATH = '/test-pwa';
+var APP_PREFIX = 'testpwa_';
+var VERSION = 'version_003';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
